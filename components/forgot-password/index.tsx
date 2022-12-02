1d878d7c9cdc9d194e8d9fef4897e8b6d5dc9160
sarathcr <sarathcr9177725@gmail.com>
2022-12-02 10:30:29 +0530

    updated logout Functional

--- components/forgot-password/index.tsx
+++ components/forgot-password/index.tsx
@@ -8,6 +8,7 @@ const ForgotPassword = () => {
   const [error, setError] = useState<any>(null);
   const [isMailSent, setIsMailSent] = useState(false);
   const { forgotPassword }: any = useAuth();
+  const { logOut }: any = useAuth();
   async function submitHandler() {
     if (email) {
       try {
@@ -17,6 +18,11 @@ const ForgotPassword = () => {
       }
     }
   }
+  const handleGoBack = () => {
+    setIsMailSent(false);
+    logOut();
+    router.push("/auth/login");
+  };
 
   const mailSent = (
     <>
@@ -24,10 +30,7 @@ const ForgotPassword = () => {
       <button
         type="button"
         className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-        onClick={() => {
-          setIsMailSent(false);
-          router.push("/auth/login");
-        }}
+        onClick={handleGoBack}
       >
         Go back to login
       </button>
